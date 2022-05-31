166eeaed88638f9d73df77bbc6c82b6a03f4c5f6
dưc <nguyenngocduc0802@gmail.com>
2022-05-31 16:47:23 +0700

    lỗi

---
+++ src/Modules/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+
+function Home() {
+  return (
+    <>
+      <h1>home</h1>
+      <Button variant="contained">Default</Button>
+    </>
+  );
+}
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { Component,Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Modules/Home/Home'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+  <Fragment>
+    <Home />
+  </Fragment>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
