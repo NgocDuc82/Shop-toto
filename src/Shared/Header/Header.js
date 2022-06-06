65df15b162bbf07ee096038db42613e05b91cc06
dưc <nguyenngocduc0802@gmail.com>
2022-06-06 16:56:28 +0700

    header fix

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 function Header(){
     return (
         <div>
-            <div className="slider" style={{width: '1296px'}}>
-                <img src="https://mcdn.nhanh.vn/store/7136/bn/HOME.jpg" />
+            <div className="slider" style={{ height: '648px'}}>
+                <img src="https://mcdn.nhanh.vn/store/7136/bn/HOME.jpg" style={{width: '100%' , height: '100%'}} />
             </div>
         </div>
     )
